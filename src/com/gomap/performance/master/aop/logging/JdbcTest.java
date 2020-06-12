8331fc310ed21c0068a820bfc29b4cf340d6d498
krishna2491 <krishna2491@gmail.com>
2020-06-12 21:55:51 +0530

    code commited

--- src/com/gomap/performance/master/aop/logging/JdbcTest.java
+++ src/com/gomap/performance/master/aop/logging/JdbcTest.java
@@ -10,8 +10,9 @@ public class JdbcTest {
 		try {
 			// Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 			Class.forName("com.mysql.jdbc.Driver");
-
-			con = DriverManager.getConnection("jdbc:mysql://ec2-3-133-244-20.us-east-2.compute.amazonaws.com:3306/emf","emf_remote","emf_remote");
+			
+			//	con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ef_test","root","root");
+			con = DriverManager.getConnection("jdbc:mysql://ec2-3-133-244-20.us-east-2.compute.amazonaws.com:3306/emf_test","emf_remote","emf_remote");
 				
 			if (con != null) {
 				System.out.println("connect");
---
+++ src/com/gomap/performance/organisastion/dto/ElementMasterDto.java
@@ -0,0 +1,125 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gomap.performance.organisastion.dto;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.sun.istack.internal.NotNull;
+
+/**
+ *
+ * @author Krishnakant
+ */
+public class ElementMasterDto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private Integer elementId;
+    private String elementName;
+    private Date createdDate;
+    private Date updatedDate;
+    private Integer activateFlag;
+    
+    
+
+    public ElementMasterDto() {}
+
+
+	/**
+	 * @return the elementId
+	 */
+	public Integer getElementId() {
+		return elementId;
+	}
+
+
+	/**
+	 * @param elementId the elementId to set
+	 */
+	public void setElementId(Integer elementId) {
+		this.elementId = elementId;
+	}
+
+
+	/**
+	 * @return the elementName
+	 */
+	public String getElementName() {
+		return elementName;
+	}
+
+
+	/**
+	 * @param elementName the elementName to set
+	 */
+	public void setElementName(String elementName) {
+		this.elementName = elementName;
+	}
+
+
+	/**
+	 * @return the createdDate
+	 */
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+
+	/**
+	 * @param createdDate the createdDate to set
+	 */
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+
+	/**
+	 * @return the updatedDate
+	 */
+	public Date getUpdatedDate() {
+		return updatedDate;
+	}
+
+
+	/**
+	 * @param updatedDate the updatedDate to set
+	 */
+	public void setUpdatedDate(Date updatedDate) {
+		this.updatedDate = updatedDate;
+	}
+
+
+	/**
+	 * @return the activateFlag
+	 */
+	public Integer getActivateFlag() {
+		return activateFlag;
+	}
+
+
+	/**
+	 * @param activateFlag the activateFlag to set
+	 */
+	public void setActivateFlag(Integer activateFlag) {
+		this.activateFlag = activateFlag;
+	}
+    
+}
